a11342ed9fd7a8eaf9f6d3ff065a48e2fa0370e4
Laskee <sebas0803@outlook.com>
2023-08-08 19:14:16 -0600

    estructuradelaconexionalaBD

---
+++ Controladores/DatosSQL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Controladores
+{
+    internal class DatosSQL
+    {
+        public SqlConnection sqlConn;
+
+        public DatosSQL()
+        {
+            sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConexionSQL"].ConnectionString);
+        }
+
+
+    }
+}
